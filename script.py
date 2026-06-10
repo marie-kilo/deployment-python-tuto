696cdd2f187e3d50ca9bd6e8ece24d5c03048aec
marie-kilo <kilomarinette@gmail.com>
2026-06-10 16:04:29 +0200

    created file

---
+++ script.py
@@ -0,0 +1,12 @@
+print("Please listen and don't code for now.")
+
+
+import sys
+
+print(sys.version_info)
+
+import pandas as pd
+
+df = pd.DataFrame([[i for i in range(10)] for j in range(10)])
+
+print(df)
